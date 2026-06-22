3e82517dd47de62e6ca2d635ff644afe458bbe90
abn <81274769+AceBNeato@users.noreply.github.com>
2026-06-22 10:17:06 +0800

    update route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,17 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { ThemeProvider } from "./context/ThemeContext";
+import { useEffect } from "react";
+import { CapacitorUpdater } from "@capgo/capacitor-updater";
+import { Capacitor } from "@capacitor/core";
 
 export default function App() {
+  useEffect(() => {
+    if (Capacitor.isNativePlatform()) {
+      CapacitorUpdater.notifyAppReady().catch(console.error);
+    }
+  }, []);
+
   return (
     <ThemeProvider>
       <RouterProvider router={router} />
