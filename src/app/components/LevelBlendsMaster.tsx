a8ca5427e6698cb02a7187b02defbf56eccb470e
abn <81274769+AceBNeato@users.noreply.github.com>
2026-09-03 21:47:38 +0800

    update ui error fix [x]

--- src/app/components/LevelBlendsMaster.tsx
+++ src/app/components/LevelBlendsMaster.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { X } from "lucide-react";
 import { useNavigate } from "react-router";
 import { LevelBlends } from "./LevelBlends";
 import { Button } from "./ui/button";
